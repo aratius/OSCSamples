dc2b5a6ff8cffc46a479cb05334da87c8b97c715
aratius <arata1129matsu@icloud.com>
2023-02-11 17:33:11 +0900

    ESP

--- ESP32/sendReceive/sendReceive.ino
+++ ESP32/sendReceive/sendReceive.ino
@@ -1,16 +1,15 @@
+// https://github.com/hideakitai/ArduinoOSC
 #include <ArduinoOSC.h>
 
 //WiFiルータ Settings
 const char* ssid = "synologyMain";  // ネットワーク名
 const char* pwd = "osaka30art";  // パスワード
+const IPAddress ip(192, 168, 77, 16);  // 自分のIPを決定する（pingコマンドなどで事前にIPが空いているか確認するのが望ましい） 
 const IPAddress gateway(192, 168, 77, 1);  // ゲートウェイ = ネットワークのベース
 const IPAddress subnet(255, 255, 255, 0);  // サブネット = だいたいこの値
 
-IPAddress ip(192, 168, 77, 16);  // 自分のIPを決定する（pingコマンドなどで事前にIPが空いているか確認するのが望ましい） 
-
 const char* host = "192.168.77.12";  // クライアント（送信先）IP
 const int portOutgoing = 12001;  // クライアント（送信先）ポート
-
 const int portIncomming = 12000;  // サーバ（受信）ポート
 
 bool isConnecting = false;
@@ -46,8 +45,7 @@ void setup () {
 
   if (isConnected) {
     Serial.println("INFO : Wi-Fi Connected.");
-    //受信のリスナー設定
-    OscWiFi.subscribe(portIncomming, "/app/status", onOscReceivedStatus);
+    setUpOsc();
   } else {
     Serial.println("INFO : Wi-Fi Connect Failed.");
   }
@@ -56,16 +54,49 @@ void setup () {
   isConnecting = false;
 }
 
+void setUpOsc() {
+  // 受信のリスナー設定
+  OscWiFi.subscribe(portIncomming, "/app/status", onOscReceivedStatus);
+  OscWiFi.subscribe(portIncomming, "/*", onOscReceivedAny);  // ワイルドカード
+}
+
 void onOscReceivedStatus(OscMessage& m) {
-  int statusVal = m.arg<int>(0);  // 一つ目の引数（int）を取得
-  Serial.print("onOscReceivedStatus : ");
-  Serial.println(statusVal);
+  Serial.println(m.remoteIP()/* 送信元IP */);
+  Serial.println(m.remotePort()/* 受信ポート */);
+  Serial.println(m.size()/*引数の数*/);
+  Serial.println(m.address()/*OSCアドレス*/);
+  Serial.print(m.arg<int>(0)/*1番目の引数(int)をパース*/);
+  // Serial.print(m.arg<float>(1)/*2番目の引数(float)をパース*/);
+  // Serial.print(m.arg<String>(2)/*3番目の引数(String)をパース*/);
+  
+  int someValue = m.arg<int>(0);
   // 何らかの処理
+}
+
+void onOscReceivedAny(OscMessage& m) {
+  Serial.println(m.remoteIP()/* 送信元IP */);
+  Serial.println(m.remotePort()/* 受信ポート */);
+  Serial.println(m.size()/*引数の数*/);
+  Serial.println(m.address()/*OSCアドレス*/);
+  Serial.print(m.arg<int>(0)/*1番目の引数(int)をパース*/);
+  // Serial.print(m.arg<float>(1)/*2番目の引数(float)をパース*/);
+  // Serial.print(m.arg<String>(2)/*3番目の引数(String)をパース*/);
+
+  String& address = m.address();
+
+  if(address == "/someValue") {
+    int someValue = m.arg<float>(0);
+    // 何らかの処理
+  } else if (address == "/otherValue") {
+    int otherValue = m.arg<String>(0);
+    // 何らかの処理
+  }
+
 }
 
 void loop () {
   OscWiFi.update();
-  
+
   // 例）
   // 送信実装（実際には送ってません）
   if(false) {  // ボタンが押されたら...とか
